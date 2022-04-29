ffc832856cee76dd6ec1946e12c3b8c9c6bd659d
Amatemaggio Luca <lamatemaggio@gmail.com>
2022-04-29 10:00:10 +0200

    typescript node app

---
+++ node-ts-udemy/dist/routes/todos.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const todo_1 = require("../controller/todo");
+const router = (0, express_1.Router)();
+router.post('/', todo_1.createTodo);
+router.get('/', todo_1.getTodos);
+router.patch('/:id', todo_1.updateTodos);
+router.delete('/:id', todo_1.deleteTodo);
+exports.default = router;
